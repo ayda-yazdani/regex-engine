09f56403cc05e77c63cfb695b73173f4eeaa429c
Ayda Yazdani <aydayazdani@gmail.com>
2026-01-05 16:09:11 +0000

    created the AST node definitions, necessary to capture the structure of the tokens in the abstract syntax tree, to capture the meaning in an operator acting between the literals surrounding it

--- src/ast_nodes.py
+++ src/ast_nodes.py
@@ -1,1 +1,38 @@
 # ast node definitions
+''' these are needed because when we parse a regex, 
+we don't want a flat list of tokens, we want a tree that shows 
+the structure (AST nodes are structured because they're recursive)
+'''
+
+from dataclasses import dataclass
+
+@dataclass
+class Char:
+    value: str 
+@dataclass
+class Concat:
+    left: 'RegexAST'
+    right: 'RegexAST'
+
+@dataclass
+class Union:
+    left: 'RegexAST'
+    right: 'RegexAST'
+
+@dataclass
+class Star:
+    expr: 'RegexAST'
+
+@dataclass
+class Plus:
+    expr: 'RegexAST'
+
+@dataclass
+class Optional:
+    expr: 'RegexAST'
+    
+@dataclass
+class Epsilon:
+    pass
+
+RegexAST = Char | Concat | Union | Star | Plus | Optional | Epsilon
