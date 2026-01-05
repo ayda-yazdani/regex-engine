2868c4cbf1dec5f23f7cb408144987370c074694
Ayda Yazdani <aydayazdani@gmail.com>
2026-01-05 15:12:20 +0000

    Defined TokenType and Token based on the characters the lexer will read in a string

--- src/lexer.py
+++ src/lexer.py
@@ -1,1 +1,19 @@
 # lexer - tokenize regex strings
+
+from enum import Enum, auto
+from dataclasses import dataclass
+
+class TokenType(Enum):
+    CHAR = auto()
+    STAR = auto()
+    LPAREN = auto()
+    RPAREN = auto()
+    UNION = auto()
+    PLUS = auto()
+    OPTIONAL = auto()
+    END = auto()
+
+@dataclass
+class Token:
+    type: TokenType
+    value: str | None = None
