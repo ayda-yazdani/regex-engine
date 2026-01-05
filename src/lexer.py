220a1672becd169a031c062dc7bbe99bc46f6abc
Ayda Yazdani <aydayazdani@gmail.com>
2026-01-05 15:36:25 +0000

    create tokenize() which categorises operators vs literals and outputs list of tokens for input regex string

--- src/lexer.py
+++ src/lexer.py
@@ -17,3 +17,27 @@ class TokenType(Enum):
 class Token:
     type: TokenType
     value: str | None = None
+
+
+#read each character in string, extract operator type (and value if char), and store in a list 
+def tokenize(regex: str) -> list[Token]:
+    tokens = []
+    for c in regex:
+        match c:
+            case "|":
+                tokens.append(Token(TokenType.UNION))
+            case "+":
+                tokens.append(Token(TokenType.PLUS))
+            case "*":
+                tokens.append(Token(TokenType.STAR))
+            case "(":
+                tokens.append(Token(TokenType.LPAREN))
+            case ")":
+                tokens.append(Token(TokenType.RPAREN))
+            case "?":
+                tokens.append(Token(TokenType.OPTIONAL))
+            case _:
+                tokens.append(Token(TokenType.CHAR, c))
+    
+    tokens.append(Token(TokenType.END))
+    return tokens
